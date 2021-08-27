9c83ba5b7ef3d7e4b47c8b5732fdf48d1bc8236b
AnaelRmz <anaelramirez1@hotmail.com>
2021-08-27 15:13:06 -0600

    update

--- src/app/principal/principal-routing.module.ts
+++ src/app/principal/principal-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { NotasComponent } from './pages/notas/notas.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
     {
       path: '',
       children: [
+        {path: '', redirectTo: '/principal', pathMatch: 'full'},
        { path: 'registro', component: RegistroComponent },
        { path: 'notas', component: NotasComponent},
        { path: '**', redirectTo: 'principal'}
